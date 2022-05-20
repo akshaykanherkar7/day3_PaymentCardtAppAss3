43e6f0f2baa7067a35e9d45095457cc0e84fbe83
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-05-20 21:54:05 +0530

    PaymentCard

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+// import styles from "../App.module.css";
+
+// let mainContainer = {
+
+//   // backgroundColor: `orange`,
+
+// };
+
+let aboveFlexBox = {
+  display: `flex`,
+  justifyContent: `space-between`,
+  // border: `1px solid red`,
+  padding: `30px`,
+};
+
+let head = {
+  fontSize: `40px`,
+};
+const Cards = (props) => {
+  return (
+    <div className="mainContainer">
+      {props.value.map((el) => (
+        <div
+          className="divbox"
+          style={{
+            // border: "1px solid red",
+            marginTop: "100px",
+            height: `500px`,
+            // backgroundColor: "orange",
+          }}
+        >
+          <div style={aboveFlexBox}>
+            <h3>{el.date}</h3>
+            <img
+              className="imgtag"
+              style={{ width: "90px" }}
+              src={el.logo}
+              alt=""
+            />
+          </div>
+          <div
+            style={{
+              // border: "1px solid red",
+              width: "fit-content",
+              marginLeft: "30px",
+              textAlign: "left",
+            }}
+          >
+            <button
+              style={{
+                padding: "10px 15px 10px 15px",
+                fontSize: "20px",
+                borderColor: "transparent",
+                backgroundColor: "black",
+                color: "white",
+              }}
+            >
+              Case Study
+            </button>
+            <h1 style={head}>{el.heading}</h1>
+            <h1 style={head}>{el.subheading}</h1>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "30px",
+            }}
+          >
+            <h3>{el.devices}</h3>
+            <h1> {el.sider} </h1>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
